dca097e97bf619e47913ec4b74f150bea858e6c2
Luis Contla <luis.contla.mota30@gmail.com>
2025-01-02 14:53:22 -0600

    Página Nuevo Video
    Se agregó la página de Nuevo Video con los estilos adecuados.

--- public/pages/NewVideo.jsx
+++ public/pages/NewVideo.jsx
@@ -2,7 +2,51 @@ function NewVideo() {
 
     return (
         <>
-            <h1>New Video</h1>
+            <div className="nuevo-contenedor">
+                <div className="nuevo-titulo">
+                    <div className="nuevo-titulo-texto">NUEVO VIDEO</div>
+                    <div className="nuevo titulo-instrucciones">COMPLETE EL FOMULARIO PARA CREAR UNA NUEVA TARJETA DE VIDEO</div>
+                </div>
+                <div className="nuevo-formulario">
+                    <div className="nuevo-formulario-titulo">Crear Tarjeta</div>
+                    <div className="nuevo-formulario-campos">
+                        <div className="nuevo-formulario-campos-seccion">
+                            <div className="nuevo-formulario-campos-seccion-input">
+                                <label for="titulo">Título</label>
+                                <input type="text" id="titulo" name="titulo" placeholder="Ingrese el Título"></input>
+                            </div>
+                            <div className="nuevo-formulario-campos-seccion-input">
+                                <label for="categoria">Categoría</label>
+                                <select id="categoria" name="categoria">
+                                    <option disabled selected value="">Selecciona una Categoría</option>
+                                    <option value="frontend">Front End</option>
+                                    <option value="backend">Back End</option>
+                                    <option value="inovacion">Inovación y Gestión</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="nuevo-formulario-campos-seccion">
+                            <div className="nuevo-formulario-campos-seccion-input">
+                                <label for="imagen">Imágen</label>
+                                <input type="text" id="imagen" name="imagen" placeholder="Ingrese el URL de la imágen"></input>
+                            </div>
+                            <div className="nuevo-formulario-campos-seccion-input">
+                                <label for="video">Video</label>
+                                <input type="text" id="video" name="video" placeholder="Ingrese el URL del video"></input>
+                            </div>
+                        </div>
+                        <div className="nuevo-formulario-campos-seccion">
+                            <div className="nuevo-formulario-campos-seccion-input">
+                                <label for="descripcion">Descripción</label>
+                                <textarea rows="5" id="descripcion" name="descripcion" placeholder="Ingrese la descripción del video"></textarea>
+                            </div>
+                        </div>
+                        <div className="nuevo-formulario-campos-botones">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 
--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Nav />
-      <Home />
+      <NewVideo />
       <Footer />
     </>
   )
